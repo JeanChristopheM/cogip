00e18137c9e1f412466a1135bbe3f1c5bef00ffe
Jean-Christophe Molhant <jean.christophe.molhant@gmail.com>
2022-02-14 13:46:12 +0100

    added map

--- src/components/reports/Reports.jsx
+++ src/components/reports/Reports.jsx
@@ -1,7 +1,18 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import YearChart from "../reusables/YearChart.jsx";
 import { mockData } from "../../mock_data.js";
+import "../../logic/leaflet";
+import "../../logic/markerCluster";
 
+const myMarker = L.icon({
+  iconUrl: require("../../mapAssets/icon.svg"),
+  iconSize: [20, 25],
+  iconAnchor: [10, 13],
+  popupAnchor: [0, -12],
+  shadowUrl: null,
+  shadowSize: [68, 95],
+  shadowAnchor: [22, 94],
+});
 const Reports = ({ invoices, companies }) => {
   const [chartType, setChartType] = useState("year");
   const [year, setYear] = useState(new Date().getFullYear());
@@ -26,6 +37,32 @@ const Reports = ({ invoices, companies }) => {
   const switchChart = (chartType) => {
     setChartType(chartType);
   };
+  //. Loading Map .
+  useEffect(() => {
+    //* Making sure the map does not exist yet .
+    const container = document.querySelector(".mapContainer");
+    if (container.querySelector("#map")) return;
+    //* Creating the map div .
+    let mapDiv = document.createElement("div");
+    mapDiv.id = "map";
+    container.append(mapDiv);
+    //* Initializing the map .
+    const map = L.map("map").setView([50.632557, 5.579666], 10);
+    const attribution =
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+    const tileUrl = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
+    const tiles = L.tileLayer(tileUrl, { attribution });
+    tiles.addTo(map);
+    //* Adding markers .
+    const markers = new L.MarkerClusterGroup();
+    for (let company of companies) {
+      const coords = [company.latitude, company.longitude];
+      markers.addLayer(
+        L.marker(coords, { icon: myMarker }).bindPopup(company.name)
+      );
+    }
+    map.addLayer(markers);
+  }, []);
   //. JSX.
   return (
     <main className="reports">
@@ -78,9 +115,8 @@ const Reports = ({ invoices, companies }) => {
           {invoices.filter((el) => !el.paid).length}
         </section>
       </div>
-      <div className="card">
-        <h2>Other card</h2>
-        with some data
+      <div className="card mapContainer">
+        <h2>Companies Location</h2>
       </div>
     </main>
   );
